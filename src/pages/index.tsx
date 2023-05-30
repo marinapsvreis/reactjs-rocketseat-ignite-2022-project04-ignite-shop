d22ad3befde7f745c4f9128f4c261c9355359ef5
Marina Portugal <marinapsvreis@gmail.com>
2023-05-30 19:38:45 -0300

    :truck: Link to the product page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { stripe } from "@/lib/stripe"
 import 'keen-slider/keen-slider.min.css'
 import { useKeenSlider } from 'keen-slider/react'
 import { GetStaticProps } from "next"
+import Link from "next/link"
 import Stripe from "stripe"
 
 interface HomeProps {
@@ -28,13 +29,15 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map( product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product className="keen-slider__slide">
             <Image src={product.imageUrl} width={520} height={480} alt=""/>
             <footer>
               <strong>{product.name}</strong>
               <span>{product.price}</span>
             </footer>
           </Product>
+          </Link>
         )
       })}
     </HomeContainer>
