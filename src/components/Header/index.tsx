884fd3ba646e2f575e1aa4be99f31ad3e156a2b4
Marina Portugal <marinapsvreis@gmail.com>
2023-06-02 20:00:03 -0300

    :fire: Removing cart button from Success page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,13 +3,18 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Cart } from '../Cart';
 import { HeaderContainer } from './styles';
+import { useRouter } from 'next/router';
 export function Header() {
+    const { pathname } = useRouter()
+
+    const showCartButton = pathname !== '/success'
+
     return (
         <HeaderContainer>
         <Link href="/">
           <Image src={logoImg} alt="" />
         </Link>
-        <Cart />
+        {showCartButton && <Cart />}
       </HeaderContainer>
     )
 }
