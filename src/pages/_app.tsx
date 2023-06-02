f4066cf7b6fe9436a6ec15762e5c75fefec283df
Marina Portugal <marinapsvreis@gmail.com>
2023-06-02 18:19:50 -0300

    :sparkles: CartContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Header } from '@/components/Header';
+import { CartContextProvider } from '@/contexts/CartContext';
 import { globalStyles } from '@/styles/global';
 import { Container } from '@/styles/pages/app';
 import type { AppProps } from 'next/app';
@@ -6,9 +7,11 @@ import type { AppProps } from 'next/app';
 globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Container>
-      <Header/>
-      <Component {...pageProps} />
-    </Container>
+    <CartContextProvider>
+      <Container>
+        <Header/>
+        <Component {...pageProps} />
+      </Container>
+    </CartContextProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,14 +9,11 @@ import { GetStaticProps } from "next"
 import Link from "next/link"
 import Stripe from "stripe"
 import { CartButton } from "@/components/CartButton"
+import { useCart } from "@/hooks/useCart"
+import { IProduct } from "@/contexts/CartContext"
 
 interface HomeProps {
-  products: {
-    id: string;
-    name: string;
-    imageUrl: string;
-    price: string;
-  }[]
+  products: IProduct[]
 }
 
 export default function Home({ products }: HomeProps) {
@@ -25,6 +22,8 @@ export default function Home({ products }: HomeProps) {
     skipSnaps: false,
     dragFree: true
   })
+
+  const { addToCart } = useCart()
   
   return (
     <>
@@ -46,7 +45,7 @@ export default function Home({ products }: HomeProps) {
                     <strong>{product.name}</strong>
                     <span>{product.price}</span>
                   </div>
-                  <CartButton color="green" size="large" />
+                  <CartButton color="green" size="large" onClick={() => addToCart(product)}/>
                 </footer>
               </Product>
               </Link>
